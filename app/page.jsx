d1eac716076379fc018aaa80f7d9bb59c9bb1ab9
Abigail <abigail@picnic-media.com>
2024-12-12 15:12:03 +0000

    fixing mobile styling

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import StudioSunneLogo from "./ui/studio-sunne-logo";
 import css from "./ui/home.module.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Model } from "./components/Model";
 import { Controls } from "./components/OrbitControls";
@@ -29,6 +29,18 @@ export default function Page() {
     "",
   ];
   const [chosenTexture, setChosenTexture] = useState("");
+  const [windowWidth, setWindowWidth] = useState(typeof window !== "undefined" ? window.innerWidth : 600);
+
+  function handleWindowSizeChange() {
+    setWindowWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
 
   const handleButtonClick = () => {
     const bodyElement = document.getElementsByTagName("body");
@@ -40,79 +52,76 @@ export default function Page() {
   return (
     <>
       {/* <div className={css.menu}> */}
-        <div className={css.menuLeftContents}>
-          <div className={css.menuLeftContentsLogo}>
-            <StudioSunneLogo />
-          </div>
-          <div className={css.menuLeftContentsInfo}>
-            <p className={css.menuLeftContentsInfoText}>Creative Studio</p>
-
-            <a
-              className={css.menuLeftContentsInfoText}
-              target="_blank"
-              rel="noreferrer"
-              href="mailto:hello.studio.sunne@gmail.com"
-            >
-              <p className={css.menuLeftContentsInfoText}>
-                info@studiosunne.com
-              </p>
-            </a>
-
-            <a
-              className={css.menuLeftContentsInfoText}
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.instagram.com/studiosunne/"
-            >
-              <p className={css.menuLeftContentsInfoText}>@studiosunne</p>
-            </a>
-          </div>
+      <div className={css.menuLeftContents}>
+        <div className={css.menuLeftContentsLogo}>
+          <StudioSunneLogo />
         </div>
-        <div className={css.menuRightContents}>
-          <button
-            className={css.menuRightContentsRandomButton}
-            onClick={handleButtonClick}
+        <div className={css.menuLeftContentsInfo}>
+          <p className={css.menuLeftContentsInfoText}>Creative Studio</p>
+
+          <a
+            className={css.menuLeftContentsInfoText}
+            target="_blank"
+            rel="noreferrer"
+            href="mailto:hello.studio.sunne@gmail.com"
           >
-            click me
-          </button>
+            <p className={css.menuLeftContentsInfoText}>info@studiosunne.com</p>
+          </a>
 
-          <div className={css.menuRightContentsButtons}>
-            {textures
-              .filter((e) => e.length)
-              .map((e) => (
-                <button
-                  key={e}
-                  className={css.button}
-                  value={e}
-                  style={{ backgroundImage: `url(${e})` }}
-                  onClick={(e) => setChosenTexture(e.target.value)}
-                ></button>
-              ))}
-          </div>
+          <a
+            className={css.menuLeftContentsInfoText}
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.instagram.com/studiosunne/"
+          >
+            <p className={css.menuLeftContentsInfoText}>@studiosunne</p>
+          </a>
+        </div>
+      </div>
+      <div className={css.menuRightContents}>
+        <button
+          className={css.menuRightContentsRandomButton}
+          onClick={handleButtonClick}
+        >
+          click me
+        </button>
 
-          <div className={css.menuRightContentsButtons}>
-            {colours.map((e) => (
+        <div className={css.menuRightContentsButtons}>
+          {textures
+            .filter((e) => e.length)
+            .map((e) => (
               <button
                 key={e}
                 className={css.button}
                 value={e}
-                style={{ backgroundColor: e }}
-                onClick={(e) => {
-                  const bodyElement = document.getElementsByTagName("body");
-                  bodyElement[0].style.backgroundColor = e.target.value;
-                }}
+                style={{ backgroundImage: `url(${e})` }}
+                onClick={(e) => setChosenTexture(e.target.value)}
               ></button>
             ))}
-          </div>
         </div>
+
+        <div className={css.menuRightContentsButtons}>
+          {colours.map((e) => (
+            <button
+              key={e}
+              className={css.button}
+              value={e}
+              style={{ backgroundColor: e }}
+              onClick={(e) => {
+                const bodyElement = document.getElementsByTagName("body");
+                bodyElement[0].style.backgroundColor = e.target.value;
+              }}
+            ></button>
+          ))}
+        </div>
+      </div>
       {/* </div> */}
 
-      
       <div className={css.scene}>
         <Canvas
           camera={{
             position: [-1, 0, 6],
-            zoom: 10,
+            zoom: windowWidth > 600 ? 10 : 8,
           }}
         >
           <ambientLight color={"white"} intensity={0.6} />
