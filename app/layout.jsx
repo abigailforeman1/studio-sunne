1e12f983c6a6733e1c9aec1df016d10603cb2d5d
Abigail <abigail@picnic-media.com>
2025-01-03 18:33:16 +0000

    fixing google analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,16 +12,16 @@ export default function RootLayout({ children }) {
       <head>
         <Script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-TQ7C0XEQ1N"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
 
         <Script id="google-analytics">
-          {`
+          {`    
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      gtag('config', ${"${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}"});
-  `}
+      gtag('config', "${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}");
+          `}
         </Script>
       </head>
       <body>{children}</body>
