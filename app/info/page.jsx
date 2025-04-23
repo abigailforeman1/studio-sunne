8c4bf80690fd4ab2e3501663556568042fddb6bc
Abigail <abigail@picnic-media.com>
2025-04-23 12:56:50 +0100

    adding info page

--- app/info/page.jsx
+++ app/info/page.jsx
@@ -2,6 +2,9 @@
 import { useState, useEffect } from "react";
 import css from "../ui/info.module.css";
 import { usePathname } from "next/navigation";
+import { Canvas } from "@react-three/fiber";
+import { InfoModel } from "../components/Info-Model";
+import { Controls } from "../components/OrbitControlsInfo";
 
 export default function Page() {
   const pathname = usePathname();
@@ -16,5 +19,64 @@ export default function Page() {
     };
   }, [pathname]);
 
-  return <div className={css.info}></div>;
+  return (
+    <div className={css.info}>
+      <div className={css.sceneleft}>
+        <Canvas
+          camera={{
+            position: [-1, 0, 6],
+            zoom: 15,
+          }}
+        >
+          <ambientLight color={"white"} intensity={0.6} />
+          <directionalLight position={[0, 1, 1]} intensity={4} />
+          <InfoModel />
+          <Controls />
+        </Canvas>
+      </div>
+
+      <div className={css.sceneright}>
+        <Canvas
+          camera={{
+            position: [8, -5, 4],
+            zoom: 20,
+          }}
+        >
+          <ambientLight color={"white"} intensity={0.6} />
+          <directionalLight position={[0, 1, 1]} intensity={4} />
+          <InfoModel position={[0, 0, 0]} />
+          <Controls />
+        </Canvas>
+      </div>
+
+      <section className={css.infoContainer}>
+        <div className={css.bioContainer}>
+          <p className={css.bioText}>
+            Welcome to Studio Sunne. We create playful, interactive websites designed to stick with you. Founded by Abi - a full-stack software engineer and graphic designer with agency and startup experience, and co-founder of a design brand with clients including Vogue, Primark, and Clarks. Have an idea? I'd love to bring it to life.
+          </p>
+        </div>
+        <div className={css.clientsContainer}>
+          <div className={css.clientsList}>
+            <p>Select Clients:</p>
+            <p>Arielle Free</p>
+            <p>Juicy Yoga</p>
+            <p>Brainchild Festival</p>
+            <p>Everpress</p>
+            <p>Femme Culture</p>
+          </div>
+        </div>
+        <div className={css.servicesContainer}>
+          <div className={css.servicesList}>
+            <p>Services:</p>
+            <p>Web Design</p>
+            <p>Web Development</p>
+            <p>Creative Direction</p>
+            <p>Branding</p>
+            <p>Graphic Design</p>
+            <p>Animation</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
